bd89f62a69452a84b5aa25ff430f450029ca1df5
Felipe Duarte <felipedreis@outlook.com>
2026-03-16 19:25:10 +0000

    finished Add method. Not tested exaustivelly tho

--- ds/btree.go
+++ ds/btree.go
@@ -3,9 +3,11 @@ package ds
 import (
 	"errors"
 	"fmt"
+	"log"
 )
 
 type Key interface {
+	fmt.Stringer
 	Compare(other Key) int
 }
 type Value struct {
@@ -32,14 +34,14 @@ func makeNode(leaf bool, nodeSize int, parent *Node) *Node {
 	var child []*Node = nil
 
 	if leaf {
-		values = make([]*Value, nodeSize)
+		values = make([]*Value, 0, nodeSize)
 	} else {
-		child = make([]*Node, nodeSize+1)
+		child = make([]*Node, 0, nodeSize+1)
 	}
 
 	return &Node{
 		leaf:   leaf,
-		keys:   make([]Key, nodeSize),
+		keys:   make([]Key, 0, nodeSize),
 		child:  child,
 		values: values,
 		parent: parent,
@@ -47,7 +49,7 @@ func makeNode(leaf bool, nodeSize int, parent *Node) *Node {
 }
 
 func NewBTree(nodeSize int) *BTree {
-	var rootNode = makeNode(false, nodeSize, nil)
+	var rootNode = makeNode(true, nodeSize, nil)
 
 	return &BTree{
 		root:     rootNode,
@@ -61,32 +63,66 @@ func (b *BTree) Add(key Key, value *Value) {
 	node, idx := find(b.root, key)
 
 	if node == nil {
-		// Tree is empty, add the first leaf bellow the root
+		log.Printf("Tree is empty, adding new leaf")
 		node = makeNode(true, b.nodeSize, b.root)
 		node.insertAt(idx, key, value)
-		b.root.child[0] = node
-	} else {
-		// found an existing leaf, add the value at the right position
+		b.root.keys[0] = key
+		b.root.child[1] = node
+	} else if node.leaf {
+		log.Printf("Found a leaf node with keys %v, inserting on it at index %d", node, idx)
 		nodeSize, e := node.insertAt(idx, key, value)
 
 		if e != nil {
 			fmt.Printf("Couldn't insert value in position %d", idx)
 		}
 
-		// if after write the nodeSize overflow the leaf node, split the node
-		// and add it to it's parent
-		for nodeSize > b.nodeSize && node != nil {
-			parent := node.parent
+		b.propagateSplit(node, nodeSize)
+		// leaf does not exist, create a new one
+	} else {
+		log.Printf("Found node with keys %v empty position %d, creating new leaf node", node, idx)
+		newLeaf := makeNode(true, b.nodeSize, node)
+		newLeaf.insertAt(0, key, value)
 
-			right, key := node.split(b.nodeSize)
+		nodeSize := node.insertNode(idx, key, newLeaf)
+		b.propagateSplit(node, nodeSize)
+	}
 
-			// find the key position in the parent node
-			idx = binarySearch(parent, key)
-			nodeSize = parent.insertNode(idx, key, right)
-			node = parent
+	b.keys += 1
+}
+
+func (b *BTree) propagateSplit(node *Node, nodeSize int) {
+	// if after write the nodeSize overflow the leaf node, split the node
+	// and add it to it's parent
+	splitNode := node
+	log.Println("Checking split")
+	for nodeSize > b.nodeSize && node != nil {
+		log.Printf("Splitting node %v\n", splitNode)
+		parent := splitNode.parent
+
+		if parent == nil {
+			newRoot := makeNode(false, b.nodeSize, nil)
+
+			right, key := splitNode.split(b.nodeSize)
+
+			newRoot.keys = append(newRoot.keys, key)
+			newRoot.child = append(newRoot.child, splitNode)
+			newRoot.child = append(newRoot.child, right)
+
+			splitNode.parent = newRoot
+			right.parent = newRoot
+			b.root = newRoot
+			b.depth += 1
+			break
 		}
 
-		// TODO check if the root node has to split
+		right, key := splitNode.split(b.nodeSize)
+
+		log.Printf("Splited node %v with split key %s\n", right, key)
+
+		idx := binarySearch(parent, key)
+		log.Printf("Inserting split key at parent keys %v index %d\n", parent, idx)
+		nodeSize = parent.insertNode(idx, key, right)
+		splitNode = parent
 	}
 }
 
@@ -94,8 +130,8 @@ func (*BTree) Remove(key Key) []byte {
 	return make([]byte, 1)
 }
 
-func (*BTree) Find(key Key) []byte {
-	return make([]byte, 1)
+func (*BTree) Find(key Key) Value {
+	return Value{v: nil}
 }
 
 func binarySearch(node *Node, key Key) int {
@@ -119,13 +155,12 @@ func binarySearch(node *Node, key Key) int {
 }
 
 func find(node *Node, key Key) (*Node, int) {
-	idx := binarySearch(node, key)
-
-	if !node.leaf {
-		return find(node.child[idx], key)
-	} else {
-		return node, idx
+	if node.leaf {
+		return node, binarySearch(node, key)
 	}
+
+	idx := binarySearch(node, key)
+	return find(node.child[idx], key)
 }
 
 func (n *Node) insertAt(index int, key Key, val *Value) (int, error) {
@@ -153,22 +188,68 @@ func (n *Node) split(nodeSize int) (*Node, Key) {
 
 	if n.leaf {
 		// copy mid keys and mid values
-		right.keys = n.keys[mid:]
-		right.values = n.values[mid:]
+		right.keys = append(right.keys, n.keys[mid:]...)
+		right.values = append(right.values, n.values[mid:]...)
 		n.keys = n.keys[:mid]
 		n.values = n.values[:mid]
 	} else {
 		// copy last k keys and last k + 1 child nodes
-		right.keys = n.keys[mid+1:]
-		right.child = n.child[mid+1:]
+		// the mid key goes up to the next level
+		right.keys = append(right.keys, n.keys[mid+1:]...)
+		right.child = append(right.child, n.child[mid+1:]...)
 		n.keys = n.keys[:mid]
-		n.child = n.child[:mid]
+		n.child = n.child[:mid+1]
 	}
 
 	return right, midKey
 }
 
 func (n *Node) insertNode(index int, key Key, node *Node) int {
-	// TODO implement me
-	return 0
+	n.keys = append(n.keys, nil)
+	n.child = append(n.child, nil)
+	copy(n.keys[index+1:], n.keys[index:])
+	copy(n.child[index+2:], n.child[index+1:])
+	n.keys[index] = key
+	n.child[index+1] = node
+
+	return len(n.keys)
+}
+
+func (b *BTree) Print() {
+	fmt.Println("--- B+Tree Structure ---")
+	if b.root != nil {
+		printNode(b.root, 0)
+	}
+}
+
+func (n Node) String() string {
+	// Build a string of the keys in this node
+	nodeType := "Internal"
+	if n.leaf {
+		nodeType = "Leaf"
+	} else if n.parent == nil {
+		nodeType = "Root"
+	}
+
+	return fmt.Sprintf("%s: %v", nodeType, n.keys)
+}
+
+func printNode(n *Node, level int) {
+	// Create indentation (2 spaces per level)
+	indent := ""
+	for range level {
+		indent += "  "
+	}
+
+	// Print the node info
+	fmt.Printf("%s %v\n", indent, n)
+
+	// Recurse into children if not a leaf
+	if !n.leaf {
+		for _, child := range n.child {
+			if child != nil {
+				printNode(child, level+1)
+			}
+		}
+	}
 }
--- ds/btree_test.go
+++ ds/btree_test.go
@@ -1,16 +1,17 @@
-package ds 
+package ds
 
-import(
+import (
+	"errors"
+	"fmt"
+	"strconv"
 	"testing"
-	"encoding/binary"
 )
-
 
 type TestKey struct {
 	k int
 }
 
-func (t TestKey) Compare (other Key) int {
+func (t TestKey) Compare(other Key) int {
 	otherT := other.(TestKey)
 
 	if t.k == otherT.k {
@@ -22,17 +23,83 @@ func (t TestKey) Compare (other Key) int {
 	}
 }
 
-func TestAdd(t *testing.T){
-	bt := NewBTree(3)
-	
-	for i := 0; i < 10; i += 1 {
-		testKey := TestKey{
-			k: i,
-		}
-		v := make([]byte, 8)
-		binary.AppendVarint(v, int64(i))
+func (t TestKey) String() string {
+	return strconv.Itoa(t.k)
 
-		bt.Add(testKey, v)
+}
+
+func TestBTree_Add(t *testing.T) {
+	// 1. Setup: Create a tree with a small nodeSize (e.g., 3 or 4)
+	tree := NewBTree(3)
+
+	// 2. Define your test data
+	tests := []struct {
+		name  string
+		key   int
+		value string
+	}{
+		{"First insert, initialize root node", 10, "val10"},
+		{"Second insert", 8, "val20"},
+		{"Third insert", 5, "val5"},
+		{"Splits root node, rot key will be eight", 1, "val1"},
+		{"Insert 7 in the left leaf node", 7, "val1"},
+		{"Insert 2 in the left leaf node, splits", 2, "val2"},
+		{"Inserts 20 in the rightmost node", 20, "val20"},
+		{"Inserts 30 in the rightmost node, split it", 30, "val30"},
+		{"Inserts 25, saturate the last leaf", 25, "val25"},
+		{"Inserts 35, propagate split to the root", 35, "val35"},
+		{"Inserts 3 to the leftmost node", 3, "val3"},
+		{"Inserts 4 to the leftmost node, split", 4, "val4"},
+		{"Inserts 11 ", 11, "val11"},
+		{"Inserts 9 splits middle node", 9, "val9"},
+
+		//{"", 1, "val1"},
+		//{"", 1, "val1"},
+		// Add more here...
+	}
+
+	// 3. Execution
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tree.Add(TestKey{k: tt.key}, &Value{v: []byte(tt.value)})
+
+			tree.Print()
+
+			if err := validateTree(tree.root); err != nil {
+
+			}
+
+		})
 	}
 }
 
+func validateTree(node *Node) error {
+	if node == nil {
+		return nil
+	}
+
+	if node.leaf && len(node.child) > 0 {
+		return errors.New("Leaf node contain child pointers")
+	}
+
+	if !node.leaf && len(node.values) > 0 {
+		return errors.New("Internal node should not contain values")
+	}
+
+	for i, children := range node.child {
+		if children == nil {
+			continue
+		}
+		if children.parent != node {
+
+			errMsg := fmt.Sprintf("Inconsistent node parent relationship at node keys %v and index %d", children, i)
+			return errors.New(errMsg)
+		}
+
+		if err := validateTree(children); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
